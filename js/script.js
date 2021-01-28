f01f5513a9575246d6b2d76ed956eee88a708eba
Christian Cahuana Riveros <systemmaxima@gmail.com>
2021-01-28 00:23:47 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.btn-ver-menu').on('click', function(){
+    
+    $('.pagina-izquierda').addClass("pagina-izquierda2");
+    
+    setTimeout(function(){
+        
+        $('.pagina-derecha').addClass("pagina-derecha2");
+ 
+    }, 300)
+    
+})
+
+
+$('.btn-cerrar').on('click', function(){
+    
+    $('.pagina-derecha').removeClass("pagina-derecha2");
+    
+    setTimeout(function(){
+        
+    $('.pagina-izquierda').removeClass("pagina-izquierda2");
+ 
+    }, 300)
+    
+})
